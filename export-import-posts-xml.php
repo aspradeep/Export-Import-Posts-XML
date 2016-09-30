27839efe12f1a6598aebf6eb53e6cbc9f08fd3ae
aspradeep <a.sambandam.pradeep@gmail.com>
2016-09-30 10:23:17 +0530

    Updated Readmefiles

--- export-import-posts-xml.php
+++ export-import-posts-xml.php
@@ -17,7 +17,7 @@
  * Plugin URI:        http://kavisoftek.in/export-import-posts-xml/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Kavisoftek
+ * Author:            Pradeep
  * Author URI:        http://kavisoftek.in/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
